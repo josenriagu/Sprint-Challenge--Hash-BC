42904c377b5e36a61673b2662b00dcaf31fe3f43
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-03-13 09:26:55 +0100

    feat(python): complete ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,18 +1,40 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (HashTable, hash_table_insert, hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    # make a hashTable of length equivalent to the length of the weights array
+    ht = HashTable(len(weights) or length)
 
     """
     YOUR CODE HERE
     """
+    # insert each of the weights in the hashtable using their indices as values
+    for index, weight in enumerate(weights):
+        hash_table_insert(ht, weight, index)
+        
+    # initialize variables to hold our matching indices    
+    match_pair_index = None
+    weight_index = None
+    
+    # loop through the weights and find the matching pair index, if exists
+    for weight in weights:
+        # determine the matching digit
+        match = limit - weight
+        # retrieve match: should return None if not existing yet
+        match_pair_index = hash_table_retrieve(ht, match)
+        # if match is a weight in the hashtable
+        if match_pair_index is not None:
+            # retrieve the index of the weight from hashtable
+            weight_index = hash_table_retrieve(ht, weight)
 
+            # format responses, with highest value in first index
+            if match == weight:
+                return (1, 0)
+            elif match_pair_index > weight_index:
+                return (match_pair_index, weight_index)
+            elif match_pair_index < weight_index:
+                return (weight_index, match_pair_index)
     return None
 
 
